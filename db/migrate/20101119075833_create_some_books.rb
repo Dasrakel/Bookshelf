1372da33bece48d7cc11a0d3f73c6bff4aa37882
RickVB <red.on.uplink@gmail.com>
2010-11-19 11:19:32 +0100

    fixed create somebooks migration bug & added extra info on readme

--- db/migrate/20101119075833_create_some_books.rb
+++ db/migrate/20101119075833_create_some_books.rb
@@ -1,16 +1,18 @@
 class CreateSomeBooks < ActiveRecord::Migration
   def self.up
-    #Dummy data:
     Book.new(:bookTitle => 'Ruby Sprookjes',
              :author => 'RVB', 
-             :publisher => 'STANDAARD',
+             :publisher => 'FairyTales',
              :genre_id => 3,
-             :description => 'Wonderlijke gebeurtenissen in de Ruby wereld.').save
+             :description => 'Wonderlijke gebeurtenissen in de Ruby wereld.',
+             :customerId => 1).save
+
     Book.new(:bookTitle => 'Ruby Nachtmerries',
              :author => 'RVB',
-             :publisher => 'STANDAARD',
+             :publisher => 'Killer',
              :genre_id => 1,
-             :description => 'Frustrerende gebeurtenissen in de Ruby wereld.').save
+             :description => 'Frustrerende gebeurtenissen in de Ruby wereld.',
+             :customerId => 1).save
   end
 
   def self.down

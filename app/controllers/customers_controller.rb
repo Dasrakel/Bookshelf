251f11191b15c47992c988d23e816b5cd6bd0e8d
DasRakel <mail@dasrakel.eu>
2010-12-10 12:16:00 +0100

    Editable Userprofiles + gravatars

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,18 @@
 class CustomersController < ApplicationController
 	
-  # GET /books/1
-  # GET /books/1.xml
+  # GET /customers/1
+  # GET /customers/1.xml
   def show
   @customer = Customer.find(params[:id])
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @book }
     end
+  end
+
+  # GET /customers/1/edit
+  def edit
+    @customer = Customer.find(params[:id])
   end
 
 end
--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,5 @@
 module CustomersHelper
+  def gravatar_url_for(email)
+     "http://www.gravatar.com/avatar/" + Digest::MD5.hexdigest(email)
+  end
 end

d6bfcfeed88200244fe0e23498e30dbcb1c7f979
RickVB <red.on.uplink@gmail.com>
2011-01-10 18:50:04 +0100

    AddedSomeBooksTriedAddingCustomer

--- db/migrate/20101119075833_create_some_books.rb
+++ db/migrate/20101119075833_create_some_books.rb
@@ -1,18 +1,6 @@
 class CreateSomeBooks < ActiveRecord::Migration
   def self.up
-    Book.new(:bookTitle => 'Ruby Sprookjes',
-             :author => 'RVB', 
-             :publisher => 'FairyTales',
-             :genre_id => 3,
-             :description => 'Wonderlijke gebeurtenissen in de Ruby wereld.',
-             :customerId => 1).save
-
-    Book.new(:bookTitle => 'Ruby Nachtmerries',
-             :author => 'RVB',
-             :publisher => 'Killer',
-             :genre_id => 1,
-             :description => 'Frustrerende gebeurtenissen in de Ruby wereld.',
-             :customerId => 1).save
+  	  #Om Git problemen te vermijden doen we deze migrate later
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101210100115) do
+ActiveRecord::Schema.define(:version => 20110110164137) do
 
   create_table "books", :force => true do |t|
     t.string   "bookTitle"
